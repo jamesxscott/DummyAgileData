655980b7ac1b2ee9ae38706fa92a1053cdec0605
James Scott <jamesxscott@gmail.com>
2018-01-08 11:22:31 +0000

    Added Created Date to CumuFlow -fixes #6

--- agile_simu.py
+++ agile_simu.py
@@ -6,7 +6,7 @@ numStories=20
 numDevs=5
 lengthDevTime=100
 absenceLength=0
-CumuFlow=[[0 for j in range(numStories)] for i in range(7)] #IssueID,ReadyHrs,InProgressHrs,InReviewHrs,ReadyCmp,InProgressCmp,ReviewCmp
+CumuFlow=[[0 for j in range(numStories)] for i in range(8)] #IssueID,ReadyHrs,InProgressHrs,InReviewHrs,Created,ReadyCmp,InProgressCmp,ReviewCmp
 DevTime=[[0 for j in range(lengthDevTime)] for i in range(numDevs+5)] # + Date, CountGrooming,CountWIP,CountReview,CountDone
 startDate=datetime.datetime(2017,12,1)
 InProgress_From=8
@@ -15,9 +15,9 @@ Review_From=2
 Review_To=8
 
 def printCumuFlow():
-    print "IssueID,ReadyHrs,InProgressHrs,InReviewHrs,ReadyCmp,InProgressCmp,ReviewCmp"
+    print "IssueID,ReadyHrs,InProgressHrs,InReviewHrs,Created,ReadyCmp,InProgressCmp,ReviewCmp"
     for j in range(20):
-      print "%s,%s,%s,%s,%s,%s,%s"  %(CumuFlow[0][j],CumuFlow[1][j],CumuFlow[2][j],CumuFlow[3][j],CumuFlow[4][j],CumuFlow[5][j],CumuFlow[6][j])
+      print "%s,%s,%s,%s,%s,%s,%s,%s"  %(CumuFlow[0][j],CumuFlow[1][j],CumuFlow[2][j],CumuFlow[3][j],CumuFlow[4][j],CumuFlow[5][j],CumuFlow[6][j],CumuFlow[7][j])
 
 def printDevTime():  
     print "Date, Dev#1,Dev#2,Dev#3,Dev#4,Dev#5,CountGrooming,CountWIP,CountReview,CountDone"
@@ -28,13 +28,13 @@ def workOnStories(currentDate, phase, devID):
     indexCurrentDate=DevTime[0].index(currentDate)
     if phase == "Review":
         phaseWorkColumn=3
-        phaseCompletedColumn=6
+        phaseCompletedColumn=7
     elif phase == "InProgress":
         phaseWorkColumn=2
-        phaseCompletedColumn=5
+        phaseCompletedColumn=6
     elif phase == "Groom":
         phaseWorkColumn=1
-        phaseCompletedColumn=4
+        phaseCompletedColumn=5
     else:
         return
         
@@ -60,8 +60,9 @@ def workOnStories(currentDate, phase, devID):
       
 # set up randomized values for initial stories in CumuFlow
 for i in range(len(CumuFlow[0])):
-  CumuFlow[0][i]="Story ID"+str(i+1) #assign issue ID's for initial stories
-  CumuFlow[1][i]=randint(4,16) #assign Ready times (hours) for initial stories
+  CumuFlow[0][i]="Story ID"+str(i+1)                #assign issue ID's for initial stories
+  CumuFlow[1][i]=randint(4,16)                      #assign Ready times (hours) for initial stories, between 4 and 16 hours
+  CumuFlow[4][i]=startDate.strftime('%d/%m/%Y')     #set created date for initial stories
 
  # set up randomized values for developer capacity in DevTime
 for i in range(1,len(DevTime)-4):
@@ -92,20 +93,8 @@ for j in range(0,len(DevTime[i])): #for each day
   DevTime[9][j]=sum([1 for x in CumuFlow[6] if x > 0]) #CountDone
   #if j==50:
   #  printDevTime()
-  #  os.system('pause')
-  
-  #TODO add best/worst/guess framework and output results for each
-  #TODO add blocker randomization
-  #TODO add date created column and populate it
-  #TODO add bug creation
-  #TODO add new story creation
-  #TODO add system testing and user acceptance
-  #TODO add frequency bins columns
-  #TODO after set threshold, calculate (daily) expected completion date using extrapolation of stories done rate
-  #TODO after same threshold, calculate (daily) expected completion date using frequency bins
-  #TODO add csv output
-  #TODO add graph output
-  
+  #  os.system('pause') #pause so can capture table after 50 days and work out estimate of completion date
+    
   for i in range(0,numDevs): #for each developer
       workOnStories(currentDate = DevTime[0][j], phase = "Review", devID = i+1)
       workOnStories(currentDate = DevTime[0][j], phase = "InProgress", devID = i+1)
@@ -116,4 +105,4 @@ printDevTime()
 if max(CumuFlow[1])+max(CumuFlow[2])+max(CumuFlow[3])>0:
     print "Project ran out of time"
 else:
-    print "Project completed on %s" %(max(CumuFlow[6]))
+    print "Project completed on %s" %(max(CumuFlow[7]))
